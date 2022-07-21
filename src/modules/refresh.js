12797ecc6e33f275e8f84567bf195853c615bb23
nimplay <nimrod7day@gmail.com>
2022-07-20 21:20:45 -0400

    fixing linters errors and adding .js into the import files

--- src/modules/refresh.js
+++ src/modules/refresh.js
@@ -1,11 +1,11 @@
-import Result from "./results";
+import Result from './results.js';
 
 const refresh = async () => {
   const listUl = document.querySelector('.score-table');
   const results = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/bmBsWnBXvVGZ5GV7fqzY/scores/');
   const data = await results.json();
   const arr = data.result;
-  arr.forEach((e) => {    
+  arr.forEach((e) => {
     const savedRecord = Result.record(e.user, e.score);
     listUl.innerHTML += savedRecord;
   });
